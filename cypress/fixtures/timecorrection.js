52e0c223834f6d9bb09f99153b63c2c47eca4dd5
rudywaltz <david.barkoczi@gmail.com>
2022-09-28 08:24:07 +0200

    feat: support different present type

--- cypress/fixtures/timecorrection.js
+++ cypress/fixtures/timecorrection.js
@@ -1,14 +1,15 @@
 module.exports = [
-  // {
-  //   day:6,
-  //   month:5,
-  //   time: [['09:00', '17:30']],
-  //   description:'Area demo, meetings'
-  // },
-  // {
-  //   day:7,
-  //   month:5,
-  //   time: [['09:00', '12:00'], ['13:30', '17:30']],
-  //   description:'LIF - personalWolke automation'
-  // }
+  {
+    day: 1,
+    month: 9,
+    time: [['09:00', '17:12']],
+    description:'in the office',
+    absent: 'present'
+  },
+  {
+    day: 2,
+    month: 9,
+    time: [['09:00', '17:12']],
+    description:'home office',
+  }
 ]
--- cypress/integration/timecorrection.spec.js
+++ cypress/integration/timecorrection.spec.js
@@ -10,16 +10,17 @@ context('PersonalWolke', () => {
     cy.get('.login-buttons .btn').click()
   })
 
-  timeCorrections.forEach(({ day, month, time, description, year = currentYear }) => {
+  timeCorrections.forEach(({ day, month, time, description, absent = 'home office', year = currentYear }) => {
     it(`TimeCorrection: ${year}-${month}-${day}: ${description}`, () => {
       cy.visit(`https://personalwolke.at/webdesk3/Zeitkorrektur$EM.proc?from_date=${day}.${month}.${year}`)
       cy.intercept('/webdesk3/Zeitkorrektur$E*').as('changeAbsentType')
 
       time.forEach(([from, to], index) => {
         cy.get(`#absence_code_${index + 1} select`)
-        .select('home office')
+        .select(absent)
 
       cy.wait('@changeAbsentType')
+
       cy.get(`#from_time_${index + 1} > .input-group > .ws-time`)
         .type(from)
       cy.get(`#to_time_${index + 1} > .input-group > .ws-time`)
@@ -30,7 +31,7 @@ context('PersonalWolke', () => {
       })
 
       cy.get('#description textarea')
-        .type(description)
+      .type(description)
       cy.get('#wf_startRequest_button')
         .click()
       cy.url({ timeout: 15000 })
