7ce57a30ddf544ac1f89e3325b42a7c1c3eb916d
rudywaltz <david.barkoczi@gmail.com>
2022-12-01 10:38:27 +0100

    fix: adjust selectors

--- cypress/integration/timecorrection.spec.js
+++ cypress/integration/timecorrection.spec.js
@@ -13,20 +13,19 @@ context('PersonalWolke', () => {
   timeCorrections.forEach(({ day, month, time, description, absent = 'home office', year = currentYear }) => {
     it(`TimeCorrection: ${year}-${month}-${day}: ${description}`, () => {
       cy.visit(`https://personalwolke.at/webdesk3/Zeitkorrektur$EM.proc?from_date=${day}.${month}.${year}`)
-      cy.intercept('/webdesk3/Zeitkorrektur$E*').as('changeAbsentType')
+      // cy.intercept('/webdesk3/Zeitkorrektur$E*').as('changeAbsentType')
 
       time.forEach(([from, to], index) => {
-        cy.get(`#absence_code_${index + 1} select`)
+        cy.get(`#timeCorrections .wd-table-row:nth-child(${index + 1}) select`)
         .select(absent)
 
-      cy.wait('@changeAbsentType')
-
-      cy.get(`#from_time_${index + 1} > .input-group > .ws-time`)
+      // cy.wait('@changeAbsentType')
+      cy.get(`#timeCorrections .wd-table-row:nth-child(${index + 1 }) div:nth-child(1) .forms-time .ws-time`)
         .type(from)
-      cy.get(`#to_time_${index + 1} > .input-group > .ws-time`)
+      cy.get(`#timeCorrections .wd-table-row:nth-child(${index + 1 }) div:nth-child(2) .forms-time .ws-time`)
         .type(to)
         if(index !== time.length - 1) {
-          cy.get('#wd-ta-tc-add-new-time-correction-row').click()
+          cy.get('#addTimeCorrection').click()
         }
       })
 
